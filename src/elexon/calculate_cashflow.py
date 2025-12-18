ce736291b4acf4b2ea8e6a68e074d8d8009db5c3
mpjuhasz <juhaszmp@gmail.com>
2025-12-18 19:08:37 +0100

    cashflow calculation improvements

--- src/elexon/calculate_cashflow.py
+++ src/elexon/calculate_cashflow.py
@@ -8,18 +8,21 @@ from src.elexon.utils import cashflow
 
 
 def run_from_config(bid_offer_folder: str, generation_folder: str, output_folder: str):
+    paths_to_process = [
+        p
+        for p in Path(bid_offer_folder).glob("*.csv")
+        if not Path(f"{output_folder}/{p.stem}.csv").exists()
+    ]
+
     for p in track(
-        Path(bid_offer_folder).glob("*.csv"),
-        total=len(list(Path(bid_offer_folder).glob("*.csv"))),
-        description="Calculating cashflow:"
+        paths_to_process,
+        description="Calculating cashflow:",
     ):
         unit = p.stem
         bo = pl.read_csv(p)
         gen = pl.read_csv(Path(generation_folder) / f"{unit}.csv")
-        if Path(f"{output_folder}/{unit}.csv").exists():
-            continue
+
         if not bo.is_empty() and not gen.is_empty():
-            print(unit)
             out = cashflow(bo, gen)
             out.write_csv(f"{output_folder}/{unit}.csv")
 
