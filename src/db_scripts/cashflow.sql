acf082f56b7811c1adbaec85e291e7e9b388bb28
mpjuhasz <juhaszmp@gmail.com>
2026-01-05 10:53:40 +0100

    merging all bm units into db

--- src/db_scripts/cashflow.sql
+++ src/db_scripts/cashflow.sql
@@ -9,5 +9,7 @@ ALTER TABLE indicative_cashflow ADD COLUMN bm_unit VARCHAR;
 UPDATE calculated_cashflow SET bm_unit = SUBSTRING(filename, 23, LENGTH(filename) - 26);
 
 
+CREATE TABLE all_bm_units AS SELECT * FROM read_json("./../../raw/bm_units.json");
+
 CREATE VIEW ic AS SELECT sum(totalCashflow) AS sumIC, bm_unit FROM indicative_cashflow GROUP BY bm_unit  ORDER BY sum(totalCashflow) DESC;
 CREATE VIEW cc AS SELECT sum(calculated_cashflow_extra + calculated_cashflow_curtailment) AS sumCC, bm_unit FROM calculated_cashflow GROUP BY bm_unit  ORDER BY sum(calculated_cashflow_curtailment + calculated_cashflow_extra) DESC;
